7878125a09748cb86095dcdc4f29c13062c223d9
Veljko Tekelerović <veljko.tekelerovic@gmail.com>
2020-01-13 20:40:39 +0100

    Tutorial solution ready
    - Tutorial followup and code skeleton
    - Bugfixes
    - README updates
    - Small tweaks

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# shopping-list-vue-starter
+# Demo example of Vue + Hoodie in action
+Purpose of this repository is to ilustrate usage simplicity of Hoodie offline-first framework in Vue.
+
+Repository is used as starting point for : https://www.telerik.com/blogs/offline-first-vue-apps-with-hoodie-workbox
+
+# Prerequisities
+`node` and `npm` must be installed on your machine.
---
+++ public/js/index.js
@@ -0,0 +1,122 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    name: "",
+    cost: "",
+    quantity: "",
+    items: [],
+    isLoggedIn: false
+  },
+  computed: {
+    total: function() {
+      return this.items.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.subTotal,
+        0
+      );
+    }
+  },
+  methods: {
+    toggleLoggedIn: function() {
+      this.isLoggedIn = !this.isLoggedIn;
+    },
+    onSubmit: function(event) {
+      console.log(this.name)
+      console.log(this.cost)
+      if (this.name != "" && this.cost != "" && this.quantity != "") {
+        hoodie.store.withIdPrefix("item").add({
+          name: this.name,
+          cost: this.cost,
+          quantity: this.quantity,
+          subTotal: this.cost * this.quantity
+        });
+
+        this.name = "";
+        this.cost = "";
+        this.quantity = "";
+
+        const snackbarContainer = document.querySelector("#toast");
+        snackbarContainer.MaterialSnackbar.showSnackbar({
+          message: "Item successfully added !"
+        });
+      } else {
+        const snackbarContainer = document.querySelector("#toast");
+        snackbarContainer.MaterialSnackbar.showSnackbar({
+          message: "An error occured; if statement telling than no data has been inserted @.@"
+        });
+      }//end_if
+    },
+
+    deleteRow: function(itemId) {
+      hoodie.store.withIdPrefix("item").remove(itemId);
+    },
+
+    saveList: function() {
+      hoodie.store
+        .withIdPrefix("item")
+        .findAll()
+        .then(items => {
+          //store the list
+          hoodie.store.withIdPrefix("list").add({
+            cost: this.total,
+            items: items
+          });
+
+          //delete the items
+          hoodie.store
+            .withIdPrefix("item")
+            .remove(items)
+            .then(() => {
+            //clear the table
+            this.items = [];
+
+            //notify the user
+            var snackbarContainer = document.querySelector("#toast");
+            snackbarContainer.MaterialSnackbar.showSnackbar({
+                message: "List saved successfully"
+            });
+            })
+            .catch(function(error) {
+            //notify the user
+            var snackbarContainer = document.querySelector("#toast");
+            snackbarContainer.MaterialSnackbar.showSnackbar({
+                message: error.message
+            });
+            });
+        });
+    }
+  }, 
+  created: function() {
+    hoodie.store.withIdPrefix("item").on(
+      "add",
+      item => vm.items.push(item)
+    );
+    hoodie.store.withIdPrefix("item").on(
+      "remove",
+      deletedItem =>
+        (vm.items = vm.items.filter(item => item._id !== deletedItem._id))
+    );
+
+    //retrieve items on the current list
+    console.log("Retreiving items...");
+    hoodie.store
+      .withIdPrefix("item")
+      .findAll()
+      .then(items => (vm.items = items));
+
+    hoodie.account.get("session").then(
+      function(session) {
+        // user is signed in
+        vm.isLoggedIn = true;
+
+        if (!session) {
+          // user is singed out
+          vm.isLoggedIn = false;
+        } else if (session.invalid) {
+          vm.isLoggedIn = false;
+        } else {
+          // user is signed in
+          vm.isLoggedIn = true;
+        }
+      });
+  }
+});
